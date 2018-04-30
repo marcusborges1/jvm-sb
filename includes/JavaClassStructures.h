2cbd7f314fa317d02208598dec0d9ebe69c311cc
Marcus Vinícius <marcus.f22@gmail.com>
2018-04-30 15:39:42 -0300

    Minor and Major version read working.

--- includes/JavaClassStructures.h
+++ includes/JavaClassStructures.h
@@ -4,4 +4,6 @@
 
 typedef struct {
   uint32_t 	magic_number;
+  uint16_t  minor_version;
+  uint16_t  major_version;
 } JavaClass;
--- src/ClassFileReader.c
+++ src/ClassFileReader.c
@@ -1,6 +1,7 @@
 #include "../includes/JavaClassStructures.h"
 
 JavaClass* read_class_file(char const* file_path);
+uint16_t read_2_bytes(FILE* fp);
 uint32_t read_4_bytes(FILE* fp);
 
 int main(int argc, char const *argv[]) {
@@ -18,8 +19,18 @@ JavaClass* read_class_file(char const* file_path) {
 
   JavaClass *class_file = (JavaClass *) malloc(sizeof(JavaClass));
   class_file->magic_number = read_4_bytes(file);
+  class_file->minor_version = read_2_bytes(file);
+  class_file->major_version = read_2_bytes(file);
 
-  printf("Magic Number: 0x%0X \n", class_file->magic_number);
+  printf("Magic Number: 0x%0X\n", class_file->magic_number);
+  printf("Minor Version: %d\n", class_file->minor_version);
+  printf("Major version: %d\n", class_file->major_version);
+}
+
+uint16_t read_2_bytes(FILE* fp) {
+  uint16_t bytes_read = getc(fp);
+  bytes_read = (bytes_read << 8) | (getc(fp));
+  return bytes_read;
 }
 
 uint32_t read_4_bytes(FILE* fp) {
