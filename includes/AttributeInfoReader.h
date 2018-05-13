cbd4739cc99313fc0c5026545dc587b921bf2e15
Laís Mendes <patricia.goncalves.unb@gmail.com>
2018-05-13 03:45:38 -0300

    tirando alguns arquivos com funcoes duplicadas

--- includes/AttributeInfoReader.h
+++
@@ -1,9 +0,0 @@
-#ifndef __ATTRIBUTE_INFO_H__
-#define __ATTRIBUTE_INFO_H__
-
-#include "JavaClassStructures.h"
-
-void read_attribute_info(FILE *fp, JavaClass* class_file,
-												AttributeInfo *attributes);
-
-#endif //__ATTRIBUTE_INFO_H__
