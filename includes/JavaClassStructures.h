da2c8cba79c5c673b10bc416a0212dfe13b39617
Leonardo Almeida <leonardodealm@gmail.com>
2018-06-21 19:21:51 -0300

    Add attributes structure to class file

--- includes/JavaClassStructures.h
+++ includes/JavaClassStructures.h
@@ -55,6 +55,8 @@
 // uint8_t : typedef unsigned char
 #define u1 uint8_t
 
+typedef struct AttrCode AttrCode;
+
 // constant pool information
 typedef struct {
   u1 tag;
@@ -130,12 +132,89 @@ typedef struct {
 } CpInfo;
 
 typedef struct {
+    uint16_t start_pc;
+    uint16_t length;
+    uint16_t name_index;
+    uint16_t descriptor_index;
+    uint16_t index;
+} AttrLocalVariableTableData;
+
+typedef struct {
+    uint16_t local_variable_table_length;
+    AttrLocalVariableTableData* table_data;
+} AttrLocalVariableTable;
+
+typedef struct {
+    uint16_t start_pc;
+    uint16_t line_pc;
+} AttrLineNumberTableData;
+
+typedef struct {
+    uint16_t line_number_table_length;
+    AttrLineNumberTableData* table;
+} AttrLineNumberTable;
+
+
+typedef struct {
+  uint16_t source_file_index;
+} AttrSourceFile;
+
+typedef struct {
+    uint16_t inner_class_info_index;
+    uint16_t outer_class_info_index;
+    uint16_t inner_name_index;
+    uint16_t inner_class_access_flag;
+} InnerClassData;
+
+typedef struct {
+    uint16_t number_of_classes;
+    InnerClassData* inner_class_data;
+} AttrInnerClass;
+
+typedef struct {
+    uint16_t value_index;
+} AttrConstantValue;
+
+typedef struct {
+    uint16_t number_of_exceptions;
+    uint16_t * exception_index_table;
+} AttrException;
+
+typedef struct {
+    uint16_t start_pc;
+    uint16_t end_pc;
+    uint16_t handler_pc;
+    uint16_t catch_type;
+} AttrCodeException;
+
+
+typedef struct {
     // CONSTANT_UTf8, attribute name
     u2  attribute_name_index;
     // attribute size in bytes
     u4  attribute_length;
-    u1  *info;
+    union {
+      AttrCode* code;
+      AttrConstantValue* constant_value;
+      AttrException* exception;
+      AttrInnerClass* inner_class;
+      AttrSourceFile* source_file;
+      AttrLineNumberTable* lineNumber_table;
+      AttrLocalVariableTable* local_variable_table;
+      uint8_t * info;
+    };
 } AttributeInfo;
+
+struct AttrCode {
+  uint16_t max_stack;
+  uint16_t max_locals;
+  uint32_t code_length;
+  uint8_t *code;
+  uint16_t exceptions_table_length;
+  AttrCodeException *exceptions;
+  uint16_t attr_counts;
+  AttributeInfo *attributes;
+};
 
 typedef struct {
     u2  access_flag;
