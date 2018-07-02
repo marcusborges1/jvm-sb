714b089d67f68f9df917b6be6f4bd73a67f6944c
Leonardo Almeida <leonardodealm@gmail.com>
2018-07-02 16:35:12 -0300

    Add another test class file

---
+++ test/java/Teste.java
@@ -0,0 +1,15 @@
+class Teste {
+
+  static void foo(){
+    System.out.println("foo");
+  }
+
+  public static void main(String[] args) {
+    int a = 10;
+    int b = 15;
+
+    int c = a + b;
+    int d = b - a + 2;
+    foo();
+  }
+}
