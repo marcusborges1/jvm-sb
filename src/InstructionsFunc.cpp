38f7cc4cb457bc19db6f95c5e4dee496128fb279
Gabriel Almeida <g.almeidaa.campos@gmail.com>
2018-07-03 20:07:19 -0300

    add fsub instruction

--- src/InstructionsFunc.cpp
+++ src/InstructionsFunc.cpp
@@ -8,3 +8,24 @@
 void nop(Frame *curr_frame){
     curr_frame->pc++;
 }
+
+void fsub(Frame *curr_frame){
+  float f1, f2, f3;
+  u4 a1,a2,a3;
+
+  // Get First Argument
+  a2 = curr_frame->operand_stack.top();
+  curr_frame->operand_stack.pop();
+  // Get second Argument
+  a1 = curr_frame->operand_stack.top();
+  curr_frame->operand_stack.pop();
+  // convert u4 to float
+  memcpy(&f2, &a2, sizeof(f2));
+  memcpy(&f1, &a1, sizeof(f1));
+  f3 = f1 - f2;
+  // convert float to u4
+  memcpy(&a3, &f3, sizeof(a3));
+  curr_frame->operand_stack.push(a3);
+
+  curr_frame->pc++;
+}
